0d7a4570d799613d6171a9210b8ec4ea5292a963
hasan-sagar <khsagar0512@gmail.com>
2025-02-19 00:28:49 +0600

    Implement landing page with Swiper carousel and add new images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,13 @@ export default {
     extend: {
       colors: {
         primary: "#3C50E0",
+        secondary: "#f23030",
         textdark: "rgb(141 147 165)",
         textdark2: "rgb(28 39 76)",
         backdrop: "#F3F4F6",
         textdark3: "#606882",
         textdark4: "#1c274c",
+        backgroundColor: "#E5EAF4",
       },
     },
   },
