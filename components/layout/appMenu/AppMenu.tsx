3a5762e521e32ba8a3de009839656966b0bbad9a
hasan-sagar <khsagar0512@gmail.com>
2025-02-19 00:48:59 +0600

    Refactor AppMenu component for improved readability and structure

--- components/layout/appMenu/AppMenu.tsx
+++ components/layout/appMenu/AppMenu.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React from "react";
 import { FaRegHeart } from "react-icons/fa";
 
 export default function AppMenu() {
   return (
-    <nav className="border-t border-gray-300">
+    <nav className="border-t border-gray-300 hidden lg:block">
       <div className="max-w-[1170px] mx-auto px-4 sm:px-7.5 xl:px-0 flex flex-col sm:flex-row items-center justify-between">
         <div className="w-full sm:w-auto">
           <ul className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-6">
             <li className="border-b-2 border-transparent ">
-              <a href="#" className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6">
+              <a
+                href="#"
+                className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6"
+              >
                 Home
               </a>
             </li>
             <li className="border-b-2 border-transparent ">
-              <a href="#" className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6">
+              <a
+                href="#"
+                className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6"
+              >
                 Shops
               </a>
             </li>
             <li className="border-b-2 border-transparent ">
-              <a href="#" className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6">
+              <a
+                href="#"
+                className="hover:text-blue-500 text-custom-sm font-medium text-dark flex xl:py-6"
+              >
                 Categories
               </a>
             </li>
