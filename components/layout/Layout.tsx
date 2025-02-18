872ae98fd25f6146fc1312cb5a8640559cd62c1f
ImranPritom <impritom68@gmail.com>
2025-02-18 22:45:36 +0600

    Add AppMenu and LandingPage components to AppLayout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AppNavbar from "./appNavbar/AppNavbar";
+import AppMenu from "./appMenu/AppMenu";
+import LandingPage from "./landingPage/LandingPage";
 
 export default function AppLayout() {
   return (
     <>
       <AppNavbar />
+      <AppMenu />
+      <LandingPage />
     </>
   );
 }
