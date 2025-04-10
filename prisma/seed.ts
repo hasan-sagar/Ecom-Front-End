6e3eef7073e4acca50ad992a74ea9470b999e802
ImranPritom <impritom68@gmail.com>
2025-04-10 22:01:48 +0600

    feat: update review model and seeding logic for products, add user association

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,46 +5,46 @@ const CategoriesSeedData = require("../public/demo-data/category-seed-data");
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log("Seeding Brands....");
-  const brands = Array.from({ length: 10 }, () => ({
-    brand_name: faker.company.name(),
-    brand_image_url: faker.image.url(),
-    user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
-  }));
-  await prisma.brand.createMany({
-    data: brands,
-    skipDuplicates: true,
-  });
-  console.log("Seeding brands completed!");
-  console.log("Seeding Categories.....");
-  for (const categories of CategoriesSeedData) {
-    await prisma.category.createMany({
-      data: {
-        category_name: categories.name,
-        category_image_url: categories.image,
-        user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
-      },
-      skipDuplicates: true,
-    });
-  }
-  console.log("Seeding categories completed");
-  console.log("Seeding Suppliers....");
-  for (let i = 0; i < 500; i++) {
-    await prisma.supplier.createMany({
-      data: {
-        supplier_name: faker.person.fullName(),
-        supplier_email: faker.internet.email(),
-        supplier_phone_number: faker.phone.number("+8801#########"),
-        supplier_country: faker.location.country(),
-        supplier_city: faker.location.city(),
-        supplier_company_name: faker.company.name(),
-        supplier_address: faker.location.streetAddress(),
-        user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
-      },
-      skipDuplicates: true,
-    });
-  }
-  console.log("Seeding suppliers completed");
+  // console.log("Seeding Brands....");
+  // const brands = Array.from({ length: 10 }, () => ({
+  //   brand_name: faker.company.name(),
+  //   brand_image_url: faker.image.url(),
+  //   user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
+  // }));
+  // await prisma.brand.createMany({
+  //   data: brands,
+  //   skipDuplicates: true,
+  // });
+  // console.log("Seeding brands completed!");
+  // console.log("Seeding Categories.....");
+  // for (const categories of CategoriesSeedData) {
+  //   await prisma.category.createMany({
+  //     data: {
+  //       category_name: categories.name,
+  //       category_image_url: categories.image,
+  //       user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
+  //     },
+  //     skipDuplicates: true,
+  //   });
+  // }
+  // console.log("Seeding categories completed");
+  // console.log("Seeding Suppliers....");
+  // for (let i = 0; i < 500; i++) {
+  //   await prisma.supplier.createMany({
+  //     data: {
+  //       supplier_name: faker.person.fullName(),
+  //       supplier_email: faker.internet.email(),
+  //       supplier_phone_number: faker.phone.number("+8801#########"),
+  //       supplier_country: faker.location.country(),
+  //       supplier_city: faker.location.city(),
+  //       supplier_company_name: faker.company.name(),
+  //       supplier_address: faker.location.streetAddress(),
+  //       user_id: "efe98ada-0863-4609-b0c1-9cde300e64af",
+  //     },
+  //     skipDuplicates: true,
+  //   });
+  // }
+  // console.log("Seeding suppliers completed");
 
   console.log("Seeding products...");
 
@@ -59,7 +59,7 @@ async function main() {
     const productName = faker.commerce.productName();
     const productSlug = faker.helpers.slugify(productName).toLowerCase();
 
-    const product = await prisma.product.createMany({
+    const product = await prisma.product.create({
       data: {
         product_name: productName,
         product_description: faker.commerce.productDescription(),
@@ -83,23 +83,41 @@ async function main() {
           : null,
         sale_price: parseFloat(faker.commerce.price(5, 500)),
       },
-      skipDuplicates: true,
+      // skipDuplicates: true,
     });
 
     console.log(`Created product [SL No: ${slNo}]: ${product.product_name}`);
 
-    const imageCount = faker.number.int({ min: 2, max: 5 });
-    for (let j = 0; j < imageCount; j++) {
-      await prisma.product_image.createMany({
-        data: {
-          image_url: faker.image.urlLoremFlickr({ category: "product" }),
-          product_id: product.id,
-        },
-        skipDuplicates: true,
-      });
+    // const imageCount = faker.number.int({ min: 2, max: 5 });
+    // for (let j = 0; j < imageCount; j++) {
+    //   await prisma.product_image.createMany({
+    //     data: {
+    //       image_url: faker.image.urlLoremFlickr({ category: "product" }),
+    //       product_id: product.id,
+    //     },
+    //     skipDuplicates: true,
+    //   });
 
-      console.log(`Added image ${j + 1} for product: ${product.product_name}`);
-    }
+    //   console.log(`Added image ${j + 1} for product: ${product.product_name}`);
+    // }
+
+
+    // Generate images for the product
+    const imageCount = faker.number.int({ min: 2, max: 5 });
+    const imagesData = Array.from({ length: imageCount }, () => ({
+      image_url: faker.image.urlLoremFlickr({ category: "product" }),
+      product_id: product.id, // Associate the image with the product ID
+    }));
+
+    // Bulk create product images
+    await prisma.product_image.createMany({
+      data: imagesData,
+      skipDuplicates: true,
+    });
+
+    console.log(
+      `Added ${imageCount} images for product: ${product.product_name}`
+    );
   }
 
   console.log("Product seeding completed!");
